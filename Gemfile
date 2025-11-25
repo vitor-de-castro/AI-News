0d338064130f7ece970fd33f0817a3b62961e9a5
Grenié Tom <tom.grenie@gmail.com>
2025-11-25 14:24:26 +0100

    Starting of the seed

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem "rails", "~> 7.1.6"
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
+gem 'faker'
 
 # Use postgresql as the database for Active Record
 gem "pg", "~> 1.1"
@@ -22,9 +23,11 @@ gem "turbo-rails"
 
 # Hotwire's modest JavaScript framework [https://stimulus.hotwired.dev]
 gem "stimulus-rails"
-
+gem 'json'
+gem 'open-uri'
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
+gem 'rest-client'
 
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,56 @@
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'rest-client'
+require 'json'
+require 'open-uri'
+
+categories = ["sports", "technology", "health", "science", "entertainment", "business"]
+
+
+NEWS_API_KEY = "b878c6fd89b64f04b240f60f689bc52d"
+
+#Fonction pour changer l'url en fonction de la catégorie souhaitée
+def url_change(categories)
+  if categories == "sports"
+    url = "https://newsapi.org/v2/top-headlines?category=sports&language=en&apiKey=#{NEWS_API_KEY}"
+  elsif categories == "technology"
+    url = "https://newsapi.org/v2/top-headlines?category=technology&language=en&apiKey=#{NEWS_API_KEY}"
+  elsif categories == "health"
+    url = "https://newsapi.org/v2/top-headlines?category=health&language=en&apiKey=#{NEWS_API_KEY}"
+  elsif categories == "science"
+    url = "https://newsapi.org/v2/top-headlines?category=science&language=en&apiKey=#{NEWS_API_KEY}"
+  elsif categories == "entertainment"
+    url = "https://newsapi.org/v2/top-headlines?category=entertainment&language=en&apiKey=#{NEWS_API_KEY}"
+  elsif categories == "business"
+    url = "https://newsapi.org/v2/top-headlines?category=business&language=en&apiKey=#{NEWS_API_KEY}"
+  end
+end
+
+Article.destroy_all
+puts "Nettoyage de la base de données"
+
+#Fonction pour recup les articles
+  categories.each do |category|
+    2.times do
+      get_articles = URI.parse(url_change(category)).read
+      article = JSON.parse(get_articles)
+
+      articles = article["articles"]
+      puts articles[0]
+      new_article = Article.new(
+        title: article["title"],
+        content: article["description"],
+        author: article["author"] || "Auteur inconnu",
+        date: Faker::Date.between(from: 600.days.ago, to: Date.today),
+        category: category
+      )
+      new_article.save
+    end
+  end
+
+puts "Import terminé ! #{Article.count} articles créés"
+
+#Fonction pour faire appel à l'api et parser
+
+#Fonction pour sauvegarder l'article sur la db
