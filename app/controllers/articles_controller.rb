1564081f21d23fd27a065f5cd54c22001b4c2bb1
Grenié Tom <tom.grenie@gmail.com>
2025-11-26 12:58:35 +0100

    Création de la show des article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,13 +3,13 @@ class ArticlesController < ApplicationController
   def index
     @chat = Chat.new
     if params[:category].present?
-     @articles = Article.where(category: params[:category])
+      @articles = Article.where(category: params[:category])
     else
       @articles = Article.all
     end
   end
 
-  # def show
-  #   @article = Article.find(params[:id])
-  #   @latest_articles = Article.order(created_at: :desc).limit(10)
+  def show
+    @article = Article.find(params[:id])
+  end
 end
