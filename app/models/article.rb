9d340d9abaa455854ff33d46dba852523b8e7f20
Bassam Renaud <brenaud150@gmail.com>
2025-11-27 15:27:02 +0100

    start of the dropbar

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,2 @@
 class Article < ApplicationRecord
-
-  def formatted_content
-    sentences = text.split(/(?<=[.!?])\s*/)
-    grouped = sentences.each_slice(3).map { |slice| slice.join(' ') }
-    grouped.map { |g| "<p>#{g.strip}</p>" }.join.html_safe
-  end
-
 end
